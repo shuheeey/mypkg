1f18e42dc462436d90f186eb2abb634b7439fa02
Shuhey <shuhei.daigaku@gmail.com>
2023-12-19 21:14:48 +0900

    fix

--- mypkg/listener1.py
+++ mypkg/listener1.py
@@ -2,30 +2,13 @@
 # SPDX-License-Identifier: BSB-3-Clause
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Int16
 
-class Talker(Node):
-    def __init__(self):
-        super().__init__('talker')
-        self.publisher_ = self.create_publisher(String, 'chatter', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-
-def main(args=None):
-    rclpy.init(args=args)
-    talker = Talker()
-    rclpy.spin(talker)
-    talker.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
-
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Int16, "countup", cb, 10)
+rclpy.spin(node)
