2ed282154dbcfcae5ab2d1b26229c515cdc2a9c5
Shuhey <shuhei.daigaku@gmail.com>
2023-12-19 12:00:04 +0900

    add spdx

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2023 Shuhei Yanaighori shuheidaigaku22@gmail.com
+# SPDX-License-Identifier: BSB-3-Clause
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
