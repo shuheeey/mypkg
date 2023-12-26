33ece4a4c1549536cb0256e855a1eb50d4510793
Shuhey <shuhei.daigaku@gmail.com>
2023-12-26 21:39:46 +0900

    fix

--- test/test2.bash
+++ test/test2.bash
@@ -6,7 +6,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 65 ros2 run mypkg talker1 > /tmp/mypkg.log
+timeout 10 ros2 run mypkg talker1 > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep -e '送信中' -e '終了'
+grep '送信中'
