93f00825859e0a09d7a81179eeabab15bb14aa72
Shuhey <shuhei.daigaku@gmail.com>
2023-12-26 22:58:38 +0900

    add a command

--- mypkg/listener3.py
+++ mypkg/listener3.py
@@ -1,15 +1,19 @@
+# SPDX-FileCopyrightText: 2023 Shuhei Yanaighori shuheidaigaku22@gmail.com
+# SPDX-License-Identifier: BSB-3-Clausei
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
-import time
 
 def cb(msg):
     global node
     if msg.data == 0:
         node.get_logger().info("     S T A R T     ")
-    #if input() == "":
-        #node.get_logger().info("%d 秒！" % msg.data)
-        #rclpy.shutdown()
+        node.get_logger().info("大谷の給料と俺の給料計算")
+    time = msg.data // 2
+    ootani = time * 6567
+    ore = time * 0.3
+    if msg.data % 20 == 0 and msg.data !=0:
+        node.get_logger().info("%d秒　　　大谷: %d 円   俺: %d 円   まじか" % (time, ootani, ore))
     if msg.data == 120:
         node.get_logger().info("    F I N I S H    ")
 
--- test/test1.bash
+++ test/test1.bash
@@ -9,4 +9,4 @@ source $dir/.bashrc
 timeout 5 ros2 launch mypkg talk_lis3.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep -e 'Sum: 0' -e '     S T A R T     ' -e '--- Count every 10 sec ---'
+grep -e 'Sum: 0' -e '     S T A R T     ' -e '--- Count every 10 sec ---' -e '送信中'
