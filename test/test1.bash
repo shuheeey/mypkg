432d66b6cd7d51774d34767e0c48e93476ff6025
Shuhey <shuhei.daigaku@gmail.com>
2023-12-27 02:14:47 +0900

    add test

--- test/test1.bash
+++ test/test1.bash
@@ -8,7 +8,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 5 ros2 launch mypkg talk_lis3.launch.py > /tmp/mypkg.log
+timeout 15 ros2 launch mypkg talk_lis3.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep -e 'Sum: 0' -e '     S T A R T     ' -e '--- Count every 10 sec ---' -e '送信中'
+grep -e 'Sum: 0' -e '     S T A R T     ' -e '--- Count every 10 sec ---' -e '送信中' -e '大谷の給料と俺の給料計算' -e 'Sum: 3 は素数！' -e '↑素数足された( + 2 )↑' -e '----- 10 sec elapsed -----' -e '10秒　　　大谷: 65670 円   俺: 3 円   まじか' 
